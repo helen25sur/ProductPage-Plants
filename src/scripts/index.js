4c847c939b406d6220ed7d568ea4e826a0df5ba9
Olena Surilova <elen21surilova@gmail.com>
2026-01-06 22:37:25 +0100

    fix: add closing menu due to click to item

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,16 @@
 const menuBtn = document.getElementById('burger-menu');
 const navBar = document.body.querySelector('.header_nav-bar_mobile');
 
+const linksMobileMenu = document.body.querySelectorAll('.header_nav-bar_mobile .nav-link');
+
 menuBtn.addEventListener('click', () => {
   navBar.classList.toggle('open');
   navBar.classList.toggle('closed');
+});
+
+linksMobileMenu.forEach(link => {
+  link.addEventListener('click', () => {
+    navBar.classList.add('closed');
+    navBar.classList.remove('open');
+  });
 });
